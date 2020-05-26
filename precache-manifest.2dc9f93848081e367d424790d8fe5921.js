98d88926510c31cf5e61b18a0b3f48f618ff1dec
Robert Mopia <robert.mopia@gmail.com>
2020-05-26 13:33:46 -0700

    Updates

---
+++ precache-manifest.2dc9f93848081e367d424790d8fe5921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18becc58c0390f0fbc1f1682b188e06",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "5188ac0971bc0b35e983",
+    "url": "/portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "259a3dd26e3a56a15ee5",
+    "url": "/portfolio-app/static/js/2.ff8fd642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.ff8fd642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5188ac0971bc0b35e983",
+    "url": "/portfolio-app/static/js/main.39bd7e8e.chunk.js"
+  },
+  {
+    "revision": "ed84828383915bc7a56f",
+    "url": "/portfolio-app/static/js/runtime-main.44fb9206.js"
+  }
+]);
